b27e66232b4f7468f3b9aad1df0f91ea956577b8
kromych <kromych@noreply.github.com>
2019-07-02 14:18:39 -0700

    Trim trailing whitespace

--- 00-infra/new-prj.sh
+++ 00-infra/new-prj.sh
@@ -30,14 +30,14 @@ cat <<EOT > "${PRJ_NAME}.cpp"
 
 constexpr unsigned MAX_STEPS = 20;
 
-int main(int argc, char **argv) 
+int main(int argc, char **argv)
 {
     Verilated::commandArgs(argc, argv);
 
     auto tb = std::make_unique<V${PRJ_NAME}>();
 
-    for (auto k = 0U; k < MAX_STEPS; ++k) 
-    {       
+    for (auto k = 0U; k < MAX_STEPS; ++k)
+    {
         tb->eval();
     }
 
