7c4fc91b3736d066d614a7c3010419e0c2e329d0
kromych <kromych@noreply.github.com>
2019-07-02 23:43:28 -0700

    Mux for the Go board

--- 01-mux/mux.v
+++ 01-mux/mux.v
@@ -21,10 +21,18 @@
 
 `default_nettype none
 
-module mux(i_a, i_b, i_sel, o_led);
+module mux(i_a, i_b, i_sel, o_led, o_led1, o_led2, o_led3);
     input wire i_a, i_b, i_sel;
     output wire o_led;
 
+    output wire o_led1;
+    output wire o_led2;
+    output wire o_led3;
+
     assign o_led = i_sel ? i_a : i_b;
+
+    assign o_led1 = 0;
+    assign o_led2 = 0;
+    assign o_led3 = 0;
 
 endmodule
