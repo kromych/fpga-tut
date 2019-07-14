6c14eb7a41e2f5da4f34af724ce589330ea00f4a
kromych <kromych@noreply.github.com>
2019-07-13 18:57:09 -0700

    Async assigmnent

--- 02-blinky/blinky.v
+++ 02-blinky/blinky.v
@@ -11,14 +11,23 @@ module blinky(i_clk, o_led);
 
     initial counter = 0;
 
-    always @(posedge i_clk)
-    begin
-        counter = counter + 1'b1;        
-    end
+    // Complex pattern for the lid being lit and not
+    // assign o_led[3] <= ^counter[WIDTH-1:WIDTH-3];
+
+    // Longer flashes
+    // assign o_led[3] <= |counter[WIDTH-1:WIDTH-3];
+
+    // Shorter flashes
+    // assign o_led[3] <= &counter[WIDTH-1:WIDTH-3];
 
     assign o_led[3] = counter[WIDTH-1];
     assign o_led[2] = counter[WIDTH-2];
     assign o_led[1] = counter[WIDTH-3];
     assign o_led[0] = counter[WIDTH-4];
+
+    always @(posedge i_clk)
+    begin
+        counter <= counter + 1'b1;        
+    end
 
 endmodule
