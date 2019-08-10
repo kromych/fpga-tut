426ecfe15be8167a6fa65c25d250d17bc22bd9f0
kromych <kromych@noreply.github.com>
2019-08-10 00:20:11 -0700

    An example of PLL instantiation for TinyFPGA BX

--- 03-fsm/03-shift/shift.v
+++ 03-fsm/03-shift/shift.v
@@ -15,6 +15,39 @@
         {name: 'o_led[3]',     wave: '0.........'},
     ]}
 
+An example of PLL instantiation for TinyFPGA BX:
+
+    wire clk_10mhz;
+
+    wire BYPASS;
+    wire RESETB;
+
+    SB_PLL40_CORE usb_pll_inst (
+        .REFERENCECLK(clk_16mhz_pin3),
+        .PLLOUTCORE(clk_10mhz),
+        .RESETB(RESETB),
+        .BYPASS(BYPASS)
+    );
+
+    assign BYPASS = 0;
+    assign RESETB = 1;
+
+    // Fin=16, Fout=10;
+    defparam usb_pll_inst.DIVR = 0; 
+    defparam usb_pll_inst.DIVF = 9; 
+    defparam usb_pll_inst.DIVQ = 4;
+    defparam usb_pll_inst.FILTER_RANGE = 3'b001;
+    defparam usb_pll_inst.FEEDBACK_PATH = "SIMPLE";
+    defparam usb_pll_inst.DELAY_ADJUSTMENT_MODE_FEEDBACK = "FIXED";
+    defparam usb_pll_inst.FDA_FEEDBACK = 4'b0000;
+    defparam usb_pll_inst.DELAY_ADJUSTMENT_MODE_RELATIVE = "FIXED";
+    defparam usb_pll_inst.FDA_RELATIVE = 4'b0000;
+    defparam usb_pll_inst.SHIFTREG_DIV_MODE = 2'b00;
+    defparam usb_pll_inst.PLLOUT_SELECT = "GENCLK";
+    defparam usb_pll_inst.ENABLE_ICEGATE = 1'b0;
+
+The icepll utility generates the code.
+
 */
 
 `default_nettype none
